f0b291bbfe4d43f2434c08795155807ad068d55c
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-05-30 16:56:23 -0500

    MErged

--- src/components/Menu/MenuBasic/MenuBasic.styles.js
+++ src/components/Menu/MenuBasic/MenuBasic.styles.js
@@ -1,13 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-    root: {
-      width: '100%',
-      alignItems: 'center',
-      MozBoxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)',
-      WebkitBoxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)',
-      boxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)'
-    },
-  });
+  root: {
+    width: '100%',
+    alignItems: 'center',
+    MozBoxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)',
+    WebkitBoxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)',
+    boxShadow: '0px 10px 25px 0px rgba(50, 50, 50, 0.1)',
+    position: 'fixed',
+    top: 0,
+    zIndex: 99,
+  },
+});
 
 export default useStyles;
--- src/pages/Dashboard/Home/Home.jsx
+++ src/pages/Dashboard/Home/Home.jsx
@@ -3,7 +3,14 @@ import HistoryList from './containers/HistoryList';
 
 const Home = () => {
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        paddingTop: 80,
+      }}
+    >
       <HistoryList />
     </div>
   );
