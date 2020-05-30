796efc8140996d2400a74c813157c8cbed3dd0ba
Daniel Reyes <danireyes_2@hotmail.com>
2020-05-30 12:47:03 -0500

    Initial Component

--- src/containers/test.js
+++ src/containers/test.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import ProfileComponent from '../components/Profile'
 
 const Test = () => {
     return (
-        <div>
-            <h1>Welcome to the jungle</h1>
-        </div>
+        <ProfileComponent/>
     )
 }
 
