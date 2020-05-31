4ef565c17d6493d945cf5ed16975f074357c39c8
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-05-31 15:29:55 -0500

    Ready to merge

--- src/pages/Dashboard/Profile/containers/ProfileSide/ProfileSide.jsx
+++ src/pages/Dashboard/Profile/containers/ProfileSide/ProfileSide.jsx
@@ -4,70 +4,88 @@ import SideProfileMenuComponent from '../../components/SideProfileMenu';
 import NoEditSideProfileMenuComponent from '../../components/NoEditSideProfileMenu';
 import { updateUser } from '@/redux/thunk/user.thunk';
 import { getStorage, setStorage } from '@/utils/localStorage';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 export default () => {
-    const [open, setOpen] = useState(false);
-    const [user, setUser] = useState({});
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState({});
+  const [openModal, setOpenModal] = useState(false);
+  const getUserData = () => {
+    let userData = getStorage('user-session-benice');
+    userData = JSON.parse(userData);
+    setUser(userData);
+  };
 
-    const getUserData = () => {
-        let userData = getStorage('user-session-benice');
-        userData = JSON.parse(userData);
-        setUser(userData);
-    }
+  const setUserData = (data) => {
+    user.userName = data.userName;
+    user.password = data.password;
+    user.nombre = data.nombre;
+    user.correo = data.correo;
+    setStorage('user-session-benice', JSON.stringify(user));
+    getUserData();
+  };
 
-    const setUserData = data => {
-        user.userName = data.userName;
-        user.password = data.password;
-        user.nombre = data.nombre;
-        user.correo = data.correo;
-        setStorage('user-session-benice', JSON.stringify(user));
-        getUserData();
-    }
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-    useEffect(() => {
-        getUserData();
-    }, []);
-
-    const dispatch = useDispatch();
-    const updateUserData = (dataUser) => {
-        const newUserData = {
-            usuario_id: user.usuario_id,
-            userName: dataUser.username,
-            password: dataUser.password,
-            nombre: dataUser.name,
-            correo:dataUser.mail
-        }
-        
-        const promise = dispatch(updateUser(newUserData));
-        promise
-            .then(() => {
-                setUserData(newUserData);
-            })
-            .catch((error) => {
-                console.log('error', error)
-            });
+  const dispatch = useDispatch();
+  const updateUserData = (dataUser) => {
+    const newUserData = {
+      usuario_id: user.usuario_id,
+      userName: dataUser.username,
+      password: dataUser.password,
+      nombre: dataUser.name,
+      correo: dataUser.mail,
     };
 
-    return (
-        <>
-            {
-                open ?
-                    <SideProfileMenuComponent
-                        onClick={() => setOpen(!open)}
-                        updateUserData={(data) => updateUserData(data)}
-                        name={user.nombre}
-                        mail={user.correo}
-                        username={user.userName}
-                        password={user.password}
-                    /> :
-                    <NoEditSideProfileMenuComponent
-                        onClick={() => setOpen(!open)}
-                        name={user.nombre}
-                        mail={user.correo}
-                        username={user.userName}
-                        password={user.password}
-                    />
-            }
-        </>
-    );
-}
+    const promise = dispatch(updateUser(newUserData));
+    promise
+      .then(() => {
+        setUserData(newUserData);
+        setOpenModal(true);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  };
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenModal(false);
+  };
+  return (
+    <>
+      {open ? (
+        <SideProfileMenuComponent
+          onClick={() => setOpen(!open)}
+          updateUserData={(data) => updateUserData(data)}
+          name={user.nombre}
+          mail={user.correo}
+          username={user.userName}
+          password={user.password}
+        />
+      ) : (
+        <NoEditSideProfileMenuComponent
+          onClick={() => setOpen(!open)}
+          name={user.nombre}
+          mail={user.correo}
+          username={user.userName}
+          password={user.password}
+        />
+      )}
+      <Snackbar open={openModal} autoHideDuration={3000} onClose={handleClose}>
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          severity="success"
+          onClose={handleClose}
+        >
+          User was updated
+        </MuiAlert>
+      </Snackbar>
+    </>
+  );
+};
