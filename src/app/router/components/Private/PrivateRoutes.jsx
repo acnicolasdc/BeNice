7363c5c6166611d86c39712ee8bf4f6ea9089507
miguel daza salamanca <daza_angel@hotmail.com>
2020-05-30 20:24:16 -0500

    Update PrivateRoutes.jsx

--- src/app/router/components/Private/PrivateRoutes.jsx
+++ src/app/router/components/Private/PrivateRoutes.jsx
@@ -3,14 +3,12 @@ import { Switch } from 'react-router-dom';
 import DashBoardRoutes from '@/pages/Dashboard/routes';
 import MenuBasicComponent from '@/components/Menu/MenuBasic';
 
-import PublicationRoutes from '@/pages/Publication/routes'
-
 const PrivateRoutes = ({ session, routesSchema }) => {
   return (
     <>
       <MenuBasicComponent />
       <Switch>
-        <PublicationRoutes />
+        <DashBoardRoutes />
       </Switch>
     </>
   );
